4f21528bacc64996bd7694e37a69784652fe030e
kenzaef <92014178+kenzaef@users.noreply.github.com>
2022-02-08 10:16:05 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+const carlist = [
+    { 
+        name: "Ford Fiesta",
+        category: "Category: Budget",
+        persons: 4,
+        suitcases: 1,
+        comfort: "Comfort: low",
+        image: "fordfiesta.png",
+        priceperday: 150
+    },
+    { 
+        name: "Kia Forte",
+        category: "Category: Standart",
+        persons: 5,
+        suitcases: 3,
+        comfort: "Comfort: medium",
+        image: "kiaforte.png",
+        priceperday: 200
+    },
+    { 
+        name: "Peugeot 5008",
+        category: "Category: Minivan",
+        persons: 7,
+        suitcases: 4,
+        comfort: "Comfort: high",
+        image: "peugeot5008.png",
+        priceperday: 250
+    },
+]
